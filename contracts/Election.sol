d18ab3c663ad7ab960ef25f72f73cbb29771e6d9
noonespecial009 <noonespecial009@gmail.com>
2018-08-15 15:39:46 -0700

    Basic html

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,10 +1,26 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-    //constructor stor/read candidate
-    string public candidate;
+    
+    //constructor store/read candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
 
     function Election () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
     }
+
+    function addCandidate(string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
 }
