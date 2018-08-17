0813f64c1f57099e35c35472085a3cb2245ae398
noonespecial009 <noonespecial009@gmail.com>
2018-08-16 17:26:52 -0700

    Level 3

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,8 @@ contract Election {
         string name;
         uint voteCount;
     }
+    //STORE accounts that have voted
+    mapping(address => bool) public voters;
 
     mapping(uint => Candidate) public candidates;
 
@@ -21,6 +23,22 @@ contract Election {
     function addCandidate(string _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        //require voter hasn't voted before
+        require(!voters[msg.sender], "User already voted");
+
+        //check voting for valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid Candidate");
+        
+        //record that voter has voted
+        voters[msg.sender] = true; 
+       
+        //update candidate vote count
+        candidates[_candidateId].voteCount ++;
+
+
     }
 
 }
