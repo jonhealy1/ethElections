b932b8a6e62622efe6fc1e866a41ff158f81ff91
noonespecial009 <noonespecial009@gmail.com>
2018-08-25 16:42:29 -0700

    rinkeby

--- truffle.js
+++ truffle.js
@@ -6,6 +6,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      from: "0xC609cfFE3FE3C6C4E4D8872eF6458c09e2776900",
+      network_id: 4,
+      gas: 6712390
     }
   }
 };
