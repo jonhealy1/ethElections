1bc572360fa88271a11b927f55b5d23f5ed37faf
noonespecial009 <noonespecial009@gmail.com>
2018-08-26 00:21:56 -0700

    editing, comments

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -10,51 +10,63 @@ contract Election {
         uint voteCount;
     }
 
-    //STORE accounts that have voted
+    /* we store the accounts that have voted with this mapping 
+    from a Voter's address to a boolean indicating true if a Voter has voted
+    - each account can vote 3 times: a first place vote counts for 5 votes, 
+    a second place vote: 3, and a third place vote: 1. */
     mapping(address => bool) public voters;
     mapping(address => bool) public voters2;
     mapping(address => bool) public voters3;
 
+    //map Candidate struct to a uint for storage
     mapping(uint => Candidate) public candidates;
 
+    //uint for keeping track of the number of candidates
     uint public candidatesCount;
 
-    //address public owner;
-    //address public vote;
-    //voted event
+    //voted event emitted after an account makes one of their 3 choices
     event votedEvent (
         uint indexed _candidateId
     );
 
+    /* 3 candidates are initialized when the contract is created. 
+    I do not personally know any of these people. */
     constructor () public {
         addCandidate("Arsenio Hall");
         addCandidate("Johnny Carson");
         addCandidate("Geraldo");
     }
 
+    /* the addCandidate function takes in the name of our candidate
+    and then increase the candidatesCount variable. It then creates
+    a new Candidate struct initialized with 0 votes and adds it to
+    the candidates mapping */
     function addCandidate(string _name) public {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
+    /* the vote function handles a Voter's first choice which
+    is worth a total of five votes */
     function vote(uint _candidateId) public {
-        //require voter hasn't voted before
+        //require that a voter hasn't voted before
         require(!voters[msg.sender], "User already voted");
 
-        //check voting for valid candidate
+        //ensure that we are voting for a valid candidate
         require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid Candidate");
         
         //record that voter has voted
         voters[msg.sender] = true; 
        
-        //update candidate vote count
+        //update candidate vote count by five votes
         candidates[_candidateId].voteCount += 5;
 
         //trigger voted event
         emit votedEvent(_candidateId); 
-
     }
 
+    /* the vote2 function handles a Voter's second choice which
+    is worth a total of three votes */
     function vote2(uint _candidateId) public {
         //require voter hasn't voted before
         require(!voters2[msg.sender], "User already voted");
@@ -71,7 +83,9 @@ contract Election {
         //trigger voted event
         emit votedEvent(_candidateId); 
     }
-
+    
+    /* the vote function handles a Voter's third choice which
+    is worth a total of one vote */
     function vote3(uint _candidateId) public {
         //require voter hasn't voted before
         require(!voters3[msg.sender], "User already voted");
@@ -88,7 +102,5 @@ contract Election {
         //trigger voted event
         emit votedEvent(_candidateId); 
     }
-    
-
 }
 
